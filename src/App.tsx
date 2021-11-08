f52149919d3a4e96ac57d7ea2363938c54d85d08
khali16 <linertk@gmail.com>
2021-11-08 14:28:42 +0100

    added login component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import LogIn from "./components/Authentication/LogIn";
 import SignUp from "./components/Authentication/SignUp";
 
 function App() {
   return (
     <>
       <SignUp />
+      <LogIn />
     </>
   );
 }
--- src/Firebase/auth-context.js
+++ src/Firebase/auth-context.js
@@ -1,6 +1,11 @@
 import React, { useEffect, useContext } from "react";
 import { getFirestore, setDoc, doc } from "firebase/firestore";
-import { createUserWithEmailAndPassword, getAuth } from "@firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+} from "@firebase/auth";
+import firebase from "firebase/compat/app";
 
 export const AuthContext = React.createContext();
 
@@ -8,9 +13,18 @@ export function useAuth() {
   return useContext(AuthContext);
 }
 
+const app = firebase.initializeApp({
+  apiKey: "AIzaSyBc3jHyDBCjN1ZIhePIwxctLUtEu8_HuL8",
+  authDomain: "keep-it-cool-74626.firebaseapp.com",
+  projectId: "keep-it-cool-74626",
+  storageBucket: "keep-it-cool-74626.appspot.com",
+  messagingSenderId: "245787376748",
+  appId: "1:245787376748:web:6b744fb9e7980b6e3cac49",
+});
+
 export function AuthProvider({ children }) {
   const db = getFirestore();
-  const auth = getAuth();
+  const auth = getAuth(app);
 
   async function signUp(firstName, email, password) {
     const response = await createUserWithEmailAndPassword(
@@ -27,14 +41,19 @@ export function AuthProvider({ children }) {
     }
   }
 
+  async function logIn(email, password) {
+    await signInWithEmailAndPassword(auth, email, password);
+  }
+
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {});
 
     return unsubscribe;
-  }, []);
+  }, [auth]);
 
   const value = {
     signUp,
+    logIn,
   };
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
