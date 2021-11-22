2e7cc944159a2295972f93aa402033cb30e8d4e6
khali16 <linertk@gmail.com>
2021-11-22 15:17:17 +0100

    added all measurements component

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,5 @@
-import styles from "./App.module.css";
-import LogInWrapper from "./components/Authentication/LogInWrapper";
-import SignUpWrapper from "./components/Authentication/SignUpWrapper";
-import BeerMeasurement from "./components/Measurement/BeerMeasurement/BeerMeasurement";
-import CupOfCoffee from "./components/Measurement/CoffeeMeasurement/CupOfCoffee";
-import CupOfTea from "./components/Measurement/TeaMesurement/CupOfTea";
-import CupOfWater from "./components/Measurement/WaterMeasurement/CupOfWater";
+import AllMeasurementsLayout from "./components/Measurement/AllMeasurementsLayout";
 
-const App = () => {
-  return (
-    <div className={styles.container}>
-      <div className={styles.firstCup}>
-        <CupOfWater />
-      </div>
-      <div className={styles.secondCup}>
-        <CupOfCoffee />
-      </div>
-      <div className={styles.thirdCup}>
-        <CupOfTea />
-      </div>
-      <div className={styles.fourthCup}>
-        <BeerMeasurement />
-      </div>
-    </div>
-  );
-};
+const App = () => <AllMeasurementsLayout />;
 
 export default App;
---
+++ src/components/Measurement/AllMeasurementsLayout.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import BeerMeasurement from "./BeerMeasurement/BeerMeasurement";
+import CupOfCoffee from "./CoffeeMeasurement/CupOfCoffee";
+import CupOfTea from "./TeaMesurement/CupOfTea";
+import CupOfWater from "./WaterMeasurement/CupOfWater";
+import styles from "./AllMeasurementsLayout.module.css";
+
+const AllMeasurementsLayout = () => {
+  return (
+    <div className={styles.measurementsContainer}>
+      <div className={styles.firstCup}>
+        <CupOfWater />
+      </div>
+      <div className={styles.secondCup}>
+        <CupOfCoffee />
+      </div>
+      <div className={styles.thirdCup}>
+        <CupOfTea />
+      </div>
+      <div className={styles.fourthCup}>
+        <BeerMeasurement />
+      </div>
+    </div>
+  );
+};
+
+export default AllMeasurementsLayout;
